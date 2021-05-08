c432d6c945117e28a9b1248df5b5d183831f7493
Владислав Кузнецов <vladkusgrus@yandex.ru>
2021-05-09 01:01:38 +0500

    Tab finished

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,26 +1,15 @@
-let tabs = document.querySelectorAll('.tab');
+const tabNavigation = document.querySelector(".tab__navigation");
+const tabs = tabNavigation.querySelectorAll(".tab");
+const contents = document.querySelectorAll(".tab__content");
+tabNavigation.addEventListener("click", openTab);
 
-let tabContent = document.querySelector(".tab__content");
-
-tabs.forEach((element) => {
-	element.addEventListener('click', openTab);
-})
-
-function addActiveClass() {
-	if (this.classList.contains('tab_active')){
-		this.classList.remove('tab_active')
-	}else {
-		this.classList.add('tab_active')
-	}	
-}
-
-function openTab() {
-	let tabsArray = Array.from(tabs);
-   tabsArray.indexOf(element);
-	
-	if(tabsArray.classList.contains('tab_active')){
-		tabContent.classList.add("tab__content_active");
-	}else {
-		tabContent.classList.remove("tab__content_active");
-	}
+function openTab(event) {
+    const idx = Array.from(tabs).indexOf(event.target);
+    const activeIndex = Array.from(tabs).findIndex((tab) =>
+        tab.classList.contains("tab_active")
+    );
+    tabs[activeIndex]?.classList.remove("tab_active");
+    tabs[idx]?.classList.add("tab_active");
+    contents[activeIndex]?.classList.remove("tab__content_active");
+    contents[idx]?.classList.add("tab__content_active");
 }
